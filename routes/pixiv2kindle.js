73cb5c0fb742ab7abcf2c21ad7ab32183a770604
Koki Takahashi <hakatasiloving@gmail.com>
2016-06-26 02:34:40 +0900

    Implement long-polling example code for pixiv2kindle

--- routes/pixiv2kindle.js
+++ routes/pixiv2kindle.js
@@ -1,11 +1,37 @@
 const express = require('express');
-const router = express.Router();
+const csvStringify = require('csv-stringify');
 
 const config = require('../config.json').pixiv2kindle;
 
-router.post('/publish', function (req, res, next) {
+const router = express.Router();
+
+router.post('/publish', (req, res, next) => {
     res.set('Content-Type', 'text/csv');
-    res.end('"hoge","hoge"');
+
+    const stringifier = csvStringify();
+    stringifier.pipe(res);
+
+    stringifier.write([JSON.stringify({
+        event: 'start',
+    })]);
+
+    setTimeout(() => {
+        stringifier.write([JSON.stringify({
+            event: 'processing',
+        })]);
+
+        setTimeout(() => {
+            stringifier.write([JSON.stringify({
+                event: 'publishing',
+            })]);
+
+            setTimeout(() => {
+                stringifier.end([JSON.stringify({
+                    event: 'end',
+                })]);
+            }, 1000);
+        }, 1000);
+    }, 1000)
 });
 
 module.exports = router;
