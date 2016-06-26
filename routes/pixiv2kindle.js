b994d0f6c1051b7f36795dc459b1a1c730a30cac
Koki Takahashi <hakatasiloving@gmail.com>
2016-06-26 21:29:23 +0900

    Implement login to pixiv

--- routes/pixiv2kindle.js
+++ routes/pixiv2kindle.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const csvStringify = require('csv-stringify');
 const request = require('request');
+const cheerio = require('cheerio');
+const entities = require('entities');
+const fs = require('fs');
 
 const config = require('../config.json').pixiv2kindle;
 const jar = require('../libs/jar');
@@ -9,27 +12,76 @@ const router = express.Router();
 
 const pixivLogin = done => {
     request({
-        jar: jar,
-        method: 'POST',
-        url: 'https://www.pixiv.net/login.php',
-        form: {
-            mode: 'login',
-            pixiv_id: config.pixiv.user,
-            pass: config.pixiv.pass,
-            skip: 1,
-        },
+        jar,
+        method: 'GET',
+        url: 'https://accounts.pixiv.net/login',
     }, (error, response, body) => {
         if (error) {
             return done(error);
         }
 
-        if (response.statusCode !== 200 && response.statusCode !== 302) {
-            return done(new Error(`Status code ${response.statusCode} from login.php`));
+        if (response.statusCode !== 200) {
+            return done(new Error(`Status code ${response.statusCode} from login`));
         }
 
-        return done();
+        const $ = cheerio.load(body);
+        const key = $('input[name=post_key]').attr('value');
+
+        request({
+            jar,
+            method: 'POST',
+            url: 'https://accounts.pixiv.net/api/login?lang=ja',
+            form: {
+                pixiv_id: config.pixiv.user,
+                password: config.pixiv.pass,
+                captcha: '',
+                g_recaptcha_response: '',
+                post_key: key,
+                source: 'accounts',
+            },
+        }, (error, response, body) => {
+            if (error) {
+                return done(error);
+            }
+
+            if (response.statusCode !== 200) {
+                return done(new Error(`Status code ${response.statusCode} from login`));
+            }
+
+            return done();
+        });
     });
 };
+
+const getData = (id, done) => {
+    request({
+        jar,
+        method: 'GET',
+        url: `http://www.pixiv.net/novel/show.php?id=${id}`,
+    }, (error, response, body) => {
+        if (error || response.statusCode !== 200) {
+            return done(error);
+        }
+
+        const $ = cheerio.load(body);
+
+        if ($('body').hasClass('not-logged-in')) {
+            return done(new Error('Not logged in'));
+        }
+
+        const title = $('section.work-info > .title').text();
+        const author = $('.profile-unit .user').text();
+        const date = $('section.work-info > ul > li:nth-child(1)').text();
+        const caption = entities.decodeHTML($('.work-info > .caption').html()).replace(/<br>/g, '\n');
+        const tags = [];
+        $('li.tag > .text').each((index, element) => {
+            tags.push($(element).text());
+        });
+        const novel = $('#novel_text').text();
+
+        done(null, {title, author, date, caption, tags, novel});
+    });
+}
 
 router.post('/publish', (req, res, next) => {
     res.set('Content-Type', 'text/csv');
@@ -45,9 +97,58 @@ router.post('/publish', (req, res, next) => {
     const cookies = jar.getCookies('http://www.pixiv.net/');
     const isLoggedIn = cookies.find(cookie => cookie.key === 'PHPSESSID') !== undefined;
 
-    setTimeout(() => {
-        stringifier.end();
-    }, 1000);
+    const id = req.body.id;
+
+    if (!id) {
+        throw new Error('id not specified');
+    }
+
+    if (!isLoggedIn) {
+        pixivLogin(error => {
+            if (error) {
+                throw error;
+            }
+
+            emitEvent({event: 'Logged in to pixiv'});
+
+            getData(id, (error, data) => {
+                if (error) {
+                    throw error;
+                }
+
+                emitEvent({event: 'Retrieved Novel Data'});
+
+                console.log(data);
+            });
+        });
+    } else {
+        getData(id, (error, data) => {
+            if (error) {
+                pixivLogin(error => {
+                    if (error) {
+                        throw error;
+                    }
+
+                    emitEvent({event: 'Logged in to pixiv'});
+
+                    getData(id, (error, data) => {
+                        if (error) {
+                            throw error;
+                        }
+
+                        emitEvent({event: 'Retrieved Novel Data'});
+
+                        console.log(data);
+                    });
+                });
+                return;
+            }
+
+            emitEvent({event: 'Retrieved Novel Data'});
+
+            console.log(data);
+        })
+    }
 });
 
 module.exports = router;
