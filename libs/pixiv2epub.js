21a79c2deb141f61964cd31c4367cda6559fa432
Koki Takahashi <hakatasiloving@gmail.com>
2016-06-28 10:30:26 +0900

    Rename pixiv2html to pixiv2epub

--- libs/pixiv2epub.js
+++ libs/pixiv2epub.js
@@ -1,4 +1,6 @@
 const pixiv2html = require('pixiv2html');
+const kindlegen = require('kindlegen');
+const EventEmitter = require('events');
 
 module.exports = (text) => {
 	const html = pixiv2html(text);
