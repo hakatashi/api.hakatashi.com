82995367c129755f7534f33bce13271601644f67
Koki Takahashi <hakatasiloving@gmail.com>
2016-06-28 12:02:53 +0900

    Do not extend prototype of class

--- routes/pyoncrypt.js
+++ routes/pyoncrypt.js
@@ -21,23 +21,23 @@ if (!String.prototype.startsWith) {
 
 // attach the .equals method to Array's prototype to call it on any array
 // http://stackoverflow.com/questions/7837456/
-Array.prototype.equals = function (array) {
+equals = function (array1, array2) {
     // if the other array is a falsy value, return
-    if (!array)
+    if (!array2)
         return false;
 
     // compare lengths - can save a lot of time
-    if (this.length != array.length)
+    if (array1.length != array2.length)
         return false;
 
-    for (var i = 0, l=this.length; i < l; i++) {
+    for (var i = 0, l=array1.length; i < l; i++) {
         // Check if we have nested arrays
-        if (this[i] instanceof Array && array[i] instanceof Array) {
+        if (array1[i] instanceof Array && array2[i] instanceof Array) {
             // recurse into the nested arrays
-            if (!this[i].equals(array[i]))
+            if (!equals(array1[i], array2[i]))
                 return false;
         }
-        else if (this[i] != array[i]) {
+        else if (array1[i] != array2[i]) {
             // Warning - two different object instances will never be equal: {x:20} != {x:20}
             return false;
         }
@@ -239,7 +239,7 @@ function deserialize(buf) {
 
             for (var char in conversionTable) if (conversionTable.hasOwnProperty(char)) {
                 var record = conversionTable[char];
-                if (tokens.slice(0, record.length).equals(record)) {
+                if (equals(tokens.slice(0, record.length), record)) {
                     dest += char;
                     tokens = tokens.slice(record.length);
                     isConverted = true;
