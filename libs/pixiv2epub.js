603ae30881808df52eda4f961b89900a36544b6c
Koki Takahashi <hakatasiloving@gmail.com>
2016-11-12 03:28:27 +0900

    Implement title page and caption page

--- libs/pixiv2epub.js
+++ libs/pixiv2epub.js
@@ -4,6 +4,64 @@ const nodepub = require('nodepub');
 const path = require('path');
 const temp = require('temp');
 const fs = require('fs');
+const html = require('html-template-tag');
+
+const titlePage = ({title, author}) => html`
+	<div class="title-page">
+		<h1 class="title">${title}</h1>
+		<p class="author">${author}</p>
+	</div>
+`;
+
+const captionPage = ({title, caption, tags}) => html`
+	<div class="caption-page">
+		<h2 class="title">${title}</h2>
+		<ul class="tags">
+			${tags.map(tag => html`
+				<li class="tag">${tag}</li>
+			`)}
+		</ul>
+		<p class="caption">${[caption.replace(/\n/g, '<br>')]}</p>
+	</div>
+`;
+
+const CSS = `
+	body {
+		writing-mode: vertical-rl;
+		-webkit-writing-mode: vertical-rl;
+	}
+
+	p {
+		margin: 0;
+	}
+
+	.title-page {
+		writing-mode: horizontal-tb;
+		-webkit-writing-mode: horizontal-tb;
+		text-align: center;
+	}
+
+	.caption-page {
+		writing-mode: horizontal-tb;
+		-webkit-writing-mode: horizontal-tb;
+	}
+
+	.tags {
+		padding: 0;
+		font-family: sans-serif;
+	}
+
+	.tags::before {
+		content: 'タグ ';
+		font-weight: bold;
+	}
+
+	.tag {
+		display: inline-block;
+		margin-right: 0.5em;
+		list-style: none;
+	}
+`;
 
 class Pixiv2Epub extends EventEmitter {
 	constructor(data) {
@@ -48,11 +106,23 @@ class Pixiv2Epub extends EventEmitter {
 			images: [],
 		}, path.resolve(__dirname, '../assets/pixiv2kindle/cover.png'));
 
+		epub.addSection(`扉`, titlePage({
+			title: this.data.title,
+			author: this.data.author,
+		}));
+
+		epub.addSection(`キャプション`, captionPage({
+			title: this.data.title,
+			caption: this.data.caption,
+			date: this.data.date,
+			tags: this.data.tags,
+		}));
+
 		this.chapters.forEach((html, index) => {
 			epub.addSection(`第${index + 1}章`, html);
 		});
 
-		epub.addCSS('body {writing-mode: vertical-rl; -webkit-writing-mode: vertical-rl;} p {margin: 0;}')
+		epub.addCSS(CSS);
 
 		temp.mkdir('pixiv2kindle', (error, dirPath) => {
 			if (error) {
